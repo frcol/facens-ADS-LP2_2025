48289dc2f1da6b8fb6c762fce2d9a9f94c07b5ad
Fabio Colombini <fabiorcolombini@gmail.com>
2025-03-13 21:15:45 -0300

    ajuste

--- Aula_5-JS_POO_2/main.js
+++
@@ -1,8 +0,0 @@
-const MyClass = require('./MyClass');
-const Pessoa = require('./Pessoa');
-
-let obj = new MyClass('Fulano', 30);
-obj.imprimir();
-
-let obj2 = new Pessoa('Ciclano', '123.456.789-00');
-obj2.imprimir();
