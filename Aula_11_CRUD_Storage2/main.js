a604ad6fa28351d90bf55c175d6054d076b24ea4
Fabio Colombini <fabiorcolombini@gmail.com>
2025-05-22 21:11:34 -0300

    CRUD

---
+++ Aula_11_CRUD_Storage2/main.js
@@ -0,0 +1,8 @@
+//localStorage.setItem("username", "fabio");
+
+let user = localStorage.getItem("username");
+console.log(user);
+
+// localStorage.removeItem("username");
+
+// localStorage.clear();
